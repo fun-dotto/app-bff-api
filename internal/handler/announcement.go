3b5c16cb7a71aefbc1e74e604c65fbea9496bd65
hikaru <135803012+hikaru-0602@users.noreply.github.com>
2025-11-19 17:42:46 +0900

    Fathandler実装 (#22)
    * return mock-date
    
    * create business getdate

---
+++ internal/handler/announcement.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"time"
+
+	"github.com/fun-dotto/app-bff-api/internal/domain"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) AnnouncementsList(c *gin.Context) {
+	announcements := h.getAnnouncements()
+	c.JSON(200, announcements)
+}
+
+func (h *Handler) getAnnouncements() []domain.Announcement {
+	return h.newMockAnnouncement()
+}
+
+func (h *Handler) newMockAnnouncement() []domain.Announcement {
+	return []domain.Announcement{
+		{
+			ID:       "1",
+			Title:    "Announcement 1",
+			Date:     time.Now(),
+			URL:      "https://example.com",
+			IsActive: true,
+		},
+	}
+}
