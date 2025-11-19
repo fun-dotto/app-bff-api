364c6c35c88a9409e81fd509b1b967a7b0507e37
Hikaru Saito <beretta0602@gmail.com>
2025-11-19 18:17:48 +0900

    add comment

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,8 +4,9 @@ import "github.com/fun-dotto/app-bff-api/internal/service"
 
 type Handler struct {
 	announcementService *service.AnnouncementService
+	//今後ここに追加
 }
 
-func NewHandler(announcementService *service.AnnouncementService) *Handler {
+func NewHandler(announcementService *service.AnnouncementService /*今後ここに追加*/) *Handler {
 	return &Handler{announcementService: announcementService}
 }
