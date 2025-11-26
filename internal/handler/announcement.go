f3d7ae69911d645f718fb6fcdb3c599c7a1e7d04
hikaru <135803012+hikaru-0602@users.noreply.github.com>
2025-11-26 17:02:49 +0900

    APIの型に変換する処理，テスト追加 (#36)
    * apiの型に変更する処理追加
    
    * apiの型に変更されているかテスト追加
    
    * apiの変換処理を修正

--- internal/handler/announcement.go
+++ internal/handler/announcement.go
@@ -1,15 +1,33 @@
 package handler
 
 import (
+	"net/http"
+
+	api "github.com/fun-dotto/app-bff-api/generated"
+	"github.com/fun-dotto/app-bff-api/internal/domain"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) AnnouncementsList(c *gin.Context) {
 	announcements, err := h.announcementService.GetAnnouncements()
 	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(200, announcements)
+	apiAnnouncements := make([]api.Announcement, len(announcements))
+	for i, announcement := range announcements {
+		apiAnnouncements[i] = toApiAnnouncement(announcement)
+	}
+
+	c.JSON(http.StatusOK, apiAnnouncements)
+}
+
+func toApiAnnouncement(announcement domain.Announcement) api.Announcement {
+	return api.Announcement{
+		Id:    announcement.ID,
+		Title: announcement.Title,
+		Date:  announcement.Date,
+		Url:   announcement.URL,
+	}
 }
