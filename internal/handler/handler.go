b34efaf2175e38f1c65bc7301a3aa4a149486238
Hikaru Saito <beretta0602@gmail.com>
2026-02-28 16:44:43 +0900

    HandlerにWith関数追加

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,9 +4,27 @@ import "github.com/fun-dotto/app-bff-api/internal/service"
 
 type Handler struct {
 	announcementService *service.AnnouncementService
-	//今後ここに追加
+	subjectService      *service.SubjectService
 }
 
-func NewHandler(announcementService *service.AnnouncementService /*今後ここに追加*/) *Handler {
-	return &Handler{announcementService: announcementService}
+type Option func(*Handler)
+
+func WithAnnouncementService(s *service.AnnouncementService) Option {
+	return func(h *Handler) {
+		h.announcementService = s
+	}
+}
+
+func WithSubjectService(s *service.SubjectService) Option {
+	return func(h *Handler) {
+		h.subjectService = s
+	}
+}
+
+func NewHandler(opts ...Option) *Handler {
+	h := &Handler{}
+	for _, opt := range opts {
+		opt(h)
+	}
+	return h
 }
