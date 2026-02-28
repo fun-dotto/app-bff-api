36092224aaa895b39973ad3465c9b29d655b2a0d
Hikaru Saito <beretta0602@gmail.com>
2026-02-28 15:27:12 +0900

    簡易的なHandler実装

---
+++ internal/handler/subject.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+
+	api "github.com/fun-dotto/app-bff-api/generated"
+)
+
+func (h *Handler) SubjectsV1List(ctx context.Context, request api.SubjectsV1ListRequestObject) (api.SubjectsV1ListResponseObject, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (h *Handler) SubjectsV1Detail(ctx context.Context, request api.SubjectsV1DetailRequestObject) (api.SubjectsV1DetailResponseObject, error) {
+	return nil, fmt.Errorf("not implemented")
+}
